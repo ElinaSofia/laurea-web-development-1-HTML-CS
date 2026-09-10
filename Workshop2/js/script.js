3d38d6923a8eff60b6f22e8f160fcd6a3e259740
Elina Miasnikova <ELM00115@laurea.fi>
2026-09-10 15:39:05 +0300

    javascript harjoittelua

---
+++ Workshop2/js/script.js
@@ -0,0 +1,48 @@
+// STEP 1: Print text to the browser console is string ""
+
+console.log("Hello World!");
+
+// STEP 2: Variables
+
+// Store text values in variables
+
+const userName = "Elina";
+const favoriteAnimal ="highland baby cattle";
+
+// Print the variables to the console
+
+console.log("My name is " + userName);
+console.log("My favorite animal is " + favoriteAnimal);
+
+// STEP 3: User interaction
+
+// Display a pop-up message
+
+alert("Welcome to beautiful website");
+
+// Ask the user for their name
+
+const visitorName = prompt("What is your name? ");
+
+// Print the user's answer
+
+console.log(visitorName);
+console.log("Hello " + visitorName + "! Welcome to javascript");
+
+// Ask the user for their favorite animal
+
+const visitorfavoriteAnimal = prompt("What is your favorite animal? ");
+
+// Create a sentence using both answers
+
+console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorfavoriteAnimal + ".");
+
+// console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
+
+// ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
+if (visitorfavoriteAnimal == "highland baby cattle")
+    {console.log("Great choice! That's my favorite animal too!");} 
+// ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
+else if (visitorfavoriteAnimal !== "highland baby cattle")
+{console.log("Nice! Your favorite animal is " + visitorfavoriteAnimal);}
+// const visitorFavoriteAnimal = prompt("What is your favorite animal?");
